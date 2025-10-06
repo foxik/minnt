b18d6db3e8daacfacc37d4a56fc36da6d4303e3c
Milan Straka <milan@strakovi.com>
2025-10-06 15:53:23 +0200

    Add the Vocabulary class.

--- minnt/__init__.py
+++ minnt/__init__.py
@@ -4,6 +4,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+# Vocabulary
+from .vocabulary import Vocabulary
+
 # Utils
 from .initializers_override import global_keras_initializers
 from .startup import startup
