ce92ebb4c63f6caf59c4bfee210e9c4bfd55c0f3
Milan Straka <milan@strakovi.com>
2025-12-06 23:16:51 +0100

    Update comments.

--- minnt/__init__.py
+++ minnt/__init__.py
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# Datasets
+# TransformedDataset
 from .transformed_dataset import TransformedDataset
 
 # Type aliases
@@ -16,8 +16,6 @@ from .vocabulary import Vocabulary
 # Utils
 from .initializers_override import global_keras_initializers
 from .startup import startup
-
-# Version
 from .version import __version__
 
 # Loggers
