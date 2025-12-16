309177dafbed4f918e11511d30fac0f50ee4ca2d
Milan Straka <milan@strakovi.com>
2025-12-16 12:44:35 +0100

    Add minnt.losses.CategoricalCrossEntropy.

---
+++ docs/src/losses/categorical_cross_entropy.md
@@ -0,0 +1,3 @@
+# CategoricalCrossEntropy
+
+::: minnt.losses.CategoricalCrossEntropy
--- minnt/losses/__init__.py
+++ minnt/losses/__init__.py
@@ -4,4 +4,5 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from .binary_cross_entropy import BinaryCrossEntropy
+from .categorical_cross_entropy import CategoricalCrossEntropy
 from .mean_squared_error import MeanSquaredError
