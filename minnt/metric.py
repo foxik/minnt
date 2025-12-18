4a3bd44df9787b978a2178d2af09c57eddb5d701
Milan Straka <milan@strakovi.com>
2025-12-18 09:20:17 +0100

    Use ABC and abstractmethod in Metric.

--- minnt/metric.py
+++ minnt/metric.py
@@ -3,16 +3,17 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-import abc
+from abc import ABC, abstractmethod
 
 import torch
 
 from .type_aliases import TensorOrTensors
 
 
-class Metric(torch.nn.Module, abc.ABC):
+class Metric(torch.nn.Module, ABC):
     """An abstract metric interface."""
 
+    @abstractmethod
     def update(
         self, y: TensorOrTensors, y_true: TensorOrTensors | None = None, sample_weights: TensorOrTensors | None = None,
     ) -> None:
@@ -25,16 +26,18 @@ class Metric(torch.nn.Module, abc.ABC):
           y_true: Optional ground-truth targets.
           sample_weights: Optional sample weights.
         """
-        raise NotImplementedError()
+        ...
 
+    @abstractmethod
     def compute(self) -> torch.Tensor:
         """Compute the accumulated metric value.
 
         Returns:
           A (usually scalar) tensor representing the accumulated metric value.
         """
-        raise NotImplementedError()
+        ...
 
+    @abstractmethod
     def reset(self) -> None:
         """Reset the internal state of the metric."""
-        raise NotImplementedError()
+        ...
