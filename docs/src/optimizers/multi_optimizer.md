449c778e397ab3bfe31f4fcd43262e441ec04e91
Milan Straka <milan@strakovi.com>
2026-01-03 20:37:18 +0100

    Add minnt.optimizers.MultiOptimizer.
    Note that the MultiOptizer is currently a bit hacky and does not
    provide a full torch.optim.Optimizer functionality.

---
+++ docs/src/optimizers/multi_optimizer.md
@@ -0,0 +1,3 @@
+# MultiOptimizer
+
+::: minnt.optimizers.MultiOptimizer
--- minnt/optimizers/__init__.py
+++ minnt/optimizers/__init__.py
@@ -4,3 +4,4 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from .adafactor import Adafactor
+from .multi_optimizer import MultiOptimizer
